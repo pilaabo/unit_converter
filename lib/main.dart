34df3d4c1e506e51606cf20e509265ea58cfe1ef
Vladimir <vladdigen@gmail.com>
2024-08-07 01:04:55 +0400

    Adding the business logic

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,43 @@ class MyAppState extends State<MyApp> {
     'Pounds (lbs)',
     'Ounces',
   ];
+  final Map<String, int> _measureMap = {
+    'Meters': 0,
+    'Kilometers': 1,
+    'Grams': 2,
+    'Kilograms': 3,
+    'Feet': 4,
+    'Miles': 5,
+    'Pounds (lbs)': 6,
+    'Ounces': 7,
+  };
+  final dynamic formulas = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
+  };
+  late String _resultMessage;
+
+  void convert(double value, String from, String to) {
+    int? nFrom = _measureMap[from];
+    int? nTo = _measureMap[to];
+    var multiplier = formulas[nFrom.toString()][nTo];
+    var result = value * multiplier;
+    if (result == 0) {
+      _resultMessage = 'This conversion cannot be performed';
+    } else {
+      _resultMessage =
+      '${_numberForm.toString()} $_startMeasure are $result $_convertedMeasure';
+    }
+    setState(() {
+      _resultMessage = _resultMessage;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,7 +111,8 @@ class MyAppState extends State<MyApp> {
                     style: inputStyle,
                     value: _startMeasure,
                     items: _measures
-                        .map((value) => DropdownMenuItem<String>(
+                        .map((value) =>
+                        DropdownMenuItem<String>(
                             value: value, child: Text(value)))
                         .toList(),
                     onChanged: (value) {
@@ -94,7 +132,8 @@ class MyAppState extends State<MyApp> {
                     style: inputStyle,
                     value: _convertedMeasure,
                     items: _measures
-                        .map((value) => DropdownMenuItem<String>(
+                        .map((value) =>
+                        DropdownMenuItem<String>(
                             value: value, child: Text(value)))
                         .toList(),
                     onChanged: (value) {
@@ -105,7 +144,13 @@ class MyAppState extends State<MyApp> {
                   ),
                   SizedBox(height: 40),
                   ElevatedButton(
-                    onPressed: () => true,
+                    onPressed: () {
+                      if (_numberForm == 0) {
+                        return;
+                      } else {
+                        convert(_numberForm, _startMeasure, _convertedMeasure);
+                      }
+                    },
                     child: Text(
                       'Convert',
                       style: inputStyle,
@@ -113,7 +158,7 @@ class MyAppState extends State<MyApp> {
                   ),
                   SizedBox(height: 40),
                   Text(
-                    _numberForm == 0 ? '' : _numberForm.toString(),
+                    _resultMessage,
                     style: labelStyle,
                   ),
                   SizedBox(height: 60),
@@ -121,8 +166,8 @@ class MyAppState extends State<MyApp> {
               ),
             ),
           ),
-        ),
-      ),
+        ),)
+      ,
     );
   }
 
@@ -131,6 +176,7 @@ class MyAppState extends State<MyApp> {
     _numberForm = 0;
     _startMeasure = _measures[0];
     _convertedMeasure = _measures[1];
+    _resultMessage = '';
     super.initState();
   }
 }
